8d4f2ae1cd42c132d7a3c428fbfe4d2c7f807864
akondo02 <akondo02@students.cs.ubc.ca>
2025-07-28 21:10:47 -0700

    clean up

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,36 +3,3 @@ import LoginPage from '@/components/LoginPage';
 export default function Login() {
   return <LoginPage />;
 }
-
-
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const Login = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [token, setToken] = useState(null);
-
-//   const handleLogin = async () => {
-//     try {
-//       const response = await axios.post('https://your-backend-url.com/api/login', {
-//         username,
-//         password,
-//       });
-//       const token = response.data.token;
-//       setToken(token);
-//       // Store the token in local storage or use it to authenticate the user
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-//       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//       <button onClick={handleLogin}>Login</button>
-//       {token && <div>Welcome, {username}!</div>}
-//     </div>
-//   );
-// };
